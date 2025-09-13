4e831b4f1212586b68fe5eb8059817a1e61957e6
Adam Chandler <adam.c02pro@gmail.com>
2025-09-13 20:53:44 +0930

    feat: working container-container communication

--- discord-api-service/main.py
+++ discord-api-service/main.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv("dev.env")
 
@@ -16,5 +17,12 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+
+    response = requests.get("http://fetching-service:3000")
+    response.raise_for_status()
+    data = response.json()
+    print(data)
+
+
 
 bot.run(API_KEY)
