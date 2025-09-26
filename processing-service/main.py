f6d74cdc9a7eb4706200161c41c562303fd82b89
Adam Chandler <adam.c02pro@gmail.com>
2025-09-27 09:28:58 +0930

    Fix: changing exposed port for processing service

--- processing-service/main.py
+++ processing-service/main.py
@@ -8,4 +8,4 @@ def read_root():
     return {"message": "Processing service is running"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=3000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=3020, reload=True)
