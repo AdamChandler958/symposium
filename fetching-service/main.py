32b03d26437470a60dc43eef8487747f14e8bae1
Adam Chandler <adam.c02pro@gmail.com>
2025-09-27 17:00:09 +0930

    feat: adding query logic and basic scraping of metadata

--- fetching-service/main.py
+++ fetching-service/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 import uvicorn
 from src.logging import setup_logger
 import logging
+from src.pydantic_models import MetaDataResponse
+from src.providers import SupportedProviders
 
 setup_logger()
 logger = logging.getLogger("fetching-service")
@@ -11,6 +13,15 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Fetching service is running"}
+
+@app.get("/stream_metadata")
+def search_metadata(url: str) -> MetaDataResponse:
+    response_object = MetaDataResponse()
+    if "youtube.com" in url or "youtu.be" in url:
+        response_object.provider = SupportedProviders.YT
+    else:
+        return
+    return
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=3000, reload=True)
---
+++ fetching-service/src/pydantic_models/MetaDataResponse.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel
+
+class MetaDataResponse(BaseModel):
+    provider: str
