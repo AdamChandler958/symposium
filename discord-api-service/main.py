44daefbf4a4d4c786d3385899b39e50fff648f1c
Adam Chandler <adam.c02pro@gmail.com>
2025-09-27 10:38:18 +0930

    feat: adding logging and basic test command to symposium

--- discord-api-service/main.py
+++ discord-api-service/main.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 from src.logging import setup_logger
+import pathlib
 
 load_dotenv("dev.env")
 
@@ -16,8 +17,23 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 logger = setup_logger()
 
+async def load_cogs():
+    cogs_path = pathlib.Path("src/cogs/")
+    for cog_file in cogs_path.glob("*.py"):
+        if cog_file.name != "__init__.py":
+            module_name = str(cog_file).replace(os.sep, ".")[:-3]
+            try:
+                await bot.load_extension(module_name)
+                logger.info(f"Successfully loaded extension: {module_name}")
+            except commands.ExtensionNotFound as e:
+                logger.error(f"Failed to load extension {module_name} with error: {e}")
+
 @bot.event
 async def on_ready():
     logger.info(f"Logged in as {bot.user}")
+    await load_cogs()
+    synced = await bot.tree.sync()
+    logger.info(f"Synced {len(synced)} commands(s)!")
+    
 
 bot.run(API_KEY)
