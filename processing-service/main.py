a6a73cd1b9cc2b4ad2ee172fa7d5606946f629b7
Adam Chandler <adam.c02pro@gmail.com>
2025-09-29 15:41:38 +0930

    feat: adding more persistent connection headers to the FFMpeg stream

--- processing-service/main.py
+++ processing-service/main.py
@@ -19,7 +19,7 @@ def read_root():
 @app.get("/retrieve-audio-stream") 
 def retrieve_audio_stream(url: str):
     logger.info(f"Received request to process URL to audio for URL: {url}")
-    try:
+    try:    
         yt_stream = YouTube(url)
         audio_url = yt_stream.streams.filter(only_audio=True).first().url
 
@@ -33,19 +33,31 @@ def retrieve_audio_stream(url: str):
 
         def audio_stream_generator():
             while True:
+                if process.poll() is not None:
+                    chunk = process.stdout.read()
+                    if chunk:
+                        yield chunk
+                    break
+
                 chunk = process.stdout.read(64 * 1024) 
+                
                 if not chunk:
                     break
+                
                 yield chunk
             
-            process.wait()
+            return_code = process.wait() 
+            if return_code != 0:
+                stderr_output = process.stderr.read().decode()
+                logger.error(f"FFmpeg process exited with error code {return_code}. Stderr: {stderr_output}")
 
         return StreamingResponse(
             audio_stream_generator(),
             media_type="audio/mp3",
             headers={
                 "Content-Type": "audio/mp3", 
-                "Content-Disposition": "attachment; filename=audio.mp3"
+                "Content-Disposition": "attachment; filename=audio.mp3",
+                "Conenction": "close"
             }
         )
 
